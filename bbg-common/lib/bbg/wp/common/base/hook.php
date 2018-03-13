a654ff6b51fbabc509294d5c35da20389e18ba78
Josh Stoik <josh@blobfolio.com>
2018-03-13 08:54:07 -0700

    version bump

--- bbg-common/lib/bbg/wp/common/base/hook.php
+++ bbg-common/lib/bbg/wp/common/base/hook.php
@@ -17,7 +17,7 @@ use \blobfolio\common;
 abstract class hook {
 	// Cache-breaking string.
 
-	const ASSET_VERSION = '0.7.8';
+	const ASSET_VERSION = '0.7.9';
 
 	// Default arguments for actions.
 	const ACTION_OPTIONS = array(
