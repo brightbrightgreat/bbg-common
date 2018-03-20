9f2fafb377f0110c0488dfaf0a33734992c0ed6a
Josh Stoik <josh@blobfolio.com>
2018-03-20 14:08:35 -0700

    version bump

--- bbg-common/index.php
+++ bbg-common/index.php
@@ -3,11 +3,11 @@
  * Various tools for a happy and effective WordPress theme.
  *
  * @package BBG Common
- * @version 0.7.9
+ * @version 0.8.0
  *
  * @wordpress-plugin
  * Plugin Name: BBG Common
- * Version: 0.7.9
+ * Version: 0.8.0
  * Plugin URI: https://github.com/brightbrightgreat/bbg-common
  * Description: Various tools for a happy and effective WordPress theme.
  * Text Domain: bbg-common
